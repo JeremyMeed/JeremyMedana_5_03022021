36a1432f25a2ff66f7472399f2e704cc4841a41c
Jeremy Medana <jeremymedana@gmail.com>
2021-04-19 17:29:12 +0200

    script.js and product.js added

---
+++ product.js
@@ -0,0 +1,104 @@
+// Récupération du product id dans l'URL
+const idUrl = window.location.search;
+const urlParams = new URLSearchParams(idUrl);
+const idProduct = urlParams.get("productid");
+
+// Affichage du produit dans sa page produit
+function displayProduct(nounours) {
+  // Constantes et création des éléments
+  const productImage = document.querySelector("#product__img");
+  const productName = document.querySelector("#product__name");
+  const productDescription = document.querySelector("#product__description");
+  const productPrice = document.querySelector("#product__price");
+  const productColors = document.querySelector("#product__colors");
+  const productQuantity = document.querySelector("#product__quantity");
+  const addButton = document.querySelector("#product__add");
+
+  // Ajout attribut
+  productImage.setAttribute("src", nounours.imageUrl);
+  productImage.setAttribute("alt", `Ours en peluche ${nounours.name}`);
+
+  // Injection contenu JS dans le HTML
+  productName.innerText = nounours.name;
+  productDescription.innerText = nounours.description;
+  productPrice.innerText = `${nounours.price / 100}€`;
+
+  // Boucle pour la liste des couleurs
+  for (i = 0; i < nounours.colors.length; i++) {
+    const colorsOptions = document.createElement("option");
+    colorsOptions.innerText = nounours.colors[i];
+    colorsOptions.setAttribute("value", nounours.colors[i]);
+    productColors.appendChild(colorsOptions);
+  }
+  // Boucle pour la quantité
+  for (let i = 1; i <= 30; i++) {
+    const quantityOption = document.createElement("option");
+    quantityOption.innerText = i;
+    quantityOption.setAttribute("value", i);
+    productQuantity.appendChild(quantityOption);
+  }
+
+  //Fonction pour ajouter produit au panier
+  addButton.onclick = function () {
+    const colors = document.querySelector("#product__colors");
+    const colorSelected = colors.value;
+    const quantity = document.querySelector("#product__quantity");
+    const quantitySelected = quantity.value;
+    const idName = idProduct;
+
+    // Récupération des données du localStorage
+    const productInCart = JSON.parse(localStorage.getItem(idName));
+
+    // Fonction pour savoir si produit avec une couleur est déjà dans le localStorage
+    function isInTheCart(array, valueToDetect) {
+      for (let elem of array) {
+        if (elem.color === valueToDetect) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    // Initialise le localStorage avec l'ajout d'un premier produit
+    function initCart() {
+      const productInCart = [
+        { color: colorSelected, quantity: Number(quantitySelected) },
+      ];
+      const objLinearize = JSON.stringify(productInCart);
+      localStorage.setItem(idName, objLinearize);
+    }
+
+    // Ajouter produit identique mais couleur différente
+    function addTocart() {
+      productInCart.push({
+        color: colorSelected,
+        quantity: Number(quantitySelected),
+      });
+      const objLinearize = JSON.stringify(productInCart);
+      localStorage.setItem(idName, objLinearize);
+    }
+
+    /* ========== APPEL DES FONCTIONS ========== */
+
+    if (productInCart === null) {
+      initCart();
+    } else if (isInTheCart(productInCart, colorSelected)) {
+      productInCart.forEach((item) => {
+        if (item.color === colorSelected) {
+          item.quantity += Number(quantitySelected);
+          const objLinearize = JSON.stringify(productInCart);
+          localStorage.setItem(idName, objLinearize);
+        }
+      });
+    } else {
+      addTocart();
+    }
+  };
+}
+
+async function fillProductView() {
+  await fetch("http://localhost:3000/api/teddies/" + idProduct)
+    .then((response) => response.json()) // will return info, in json format
+    .then((nounours) => displayProduct(nounours)); // main code here, using json info
+}
+fillProductView();
---
+++ script.js
@@ -0,0 +1,41 @@
+// Cartes produits
+function fillProductsList(nounours) {
+  for (let elem of nounours) {
+    // Const et création des éléments
+    const productDiv = document.querySelector(".product__div");
+    const card = document.createElement("div");
+    const productImage = document.createElement("img");
+    const productName = document.createElement("h3");
+    const productPrice = document.createElement("p");
+    const productBtn = document.createElement("a");
+
+    // Ajout du contenu et des attributs
+    productImage.setAttribute("src", `${elem.imageUrl}`);
+    productName.innerText = `${elem.name}`;
+    productPrice.innerText = `${elem.price / 100}€`;
+    productBtn.id = elem._id;
+    productBtn.setAttribute("href", `product.html?productid=${elem._id}`);
+    productBtn.innerText = "Voir Produit";
+
+    // Ajout des classes
+    card.className = "card";
+    productName.className = "products-title";
+    productImage.className = "products-img";
+    productPrice.className = "products-text";
+    productBtn.className = "products-btn";
+
+    // Placement du contenu
+    productDiv.appendChild(card);
+    card.appendChild(productImage);
+    card.appendChild(productName);
+    card.appendChild(productPrice);
+    card.appendChild(productBtn);
+  }
+}
+
+async function fillProducts() {
+  await fetch("http://localhost:3000/api/teddies") // will return info, but in wrong format
+    .then((response) => response.json()) // will return info, in json format
+    .then((nounours) => fillProductsList(nounours)); // main code here, using json info
+}
+fillProducts();
